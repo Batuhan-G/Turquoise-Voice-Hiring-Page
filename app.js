befe8d838ab0dec9428a6e1442f1f67a73697819
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2023-01-25 01:53:15 +0300

    Added selectedItem Color and edit the data scheme

--- app.js
+++ app.js
@@ -2,127 +2,81 @@ var waitingList = [
     {
         type: "./icons/TennisRackets.png",
         rating: 3.5,
-        draw:false,
-        player1:
-            [
-                "Lynn Tanner",
-                "Roderick Flich",
-            ],
-        player2:
-            [
-                "Sledge Hammer",
-                "Willie Tanner",
-            ],
+        draw: false,
+        player1: "Lynn Tanner",
+        player2: "Roderick Flich",
+        player3: "Sledge Hammer",
+        player4: "Willie Tanner",
         time: "19"
     },
     {
         type: "./icons/TennisRackets.png",
         rating: 3.5,
-        draw:false,
-        player1:
-            [
-                "Angus MacGyver",
-                "Hannibal Smith",
-            ],
-        player2:
-            [
-                "Michael Knight",
-                "Fred Decker",
-            ],
+        draw: false,
+        player1: "Angus MacGyver",
+        player2: "Hannibal Smith",
+        player3: "Michael Knight",
+        player4: "Fred Decker",
         time: "19"
     },
     {
         type: "./icons/TennisRackets.png",
         rating: "./icons/Handshake.png",
-        draw:true,
-        player1:
-            [
-                "Thomas Magnum",
-                "Roderic Decker",
-            ],
-        player2:
-            [
-                "Tony Danza",
-                "Batuhan Gören",
-            ],
+        draw: true,
+        player1: "Thomas Magnum",
+        player2: "Roderic Decker",
+        player3: "Tony Danza",
+        player4: "Batuhan Gören",
         time: "21"
     },
     {
         type: "./icons/TennisRackets.png",
         rating: 3.5,
-        draw:false,
-        player1:
-            [
-                "Cristian Tanner",
-                "B.A. Baracus",
-            ],
-        player2:
-            [
-                "Capt. Trunk",
-                "Dori Doreau",
-            ],
+        draw: false,
+        player1:   "Cristian Tanner",
+        player2:  "B.A. Baracus",
+        player3:  "Capt. Trunk",
+        player4:  "Dori Doreau",
         time: "26"
     },
     {
         type: "./icons/TennisRackets.png",
         rating: "./icons/Handshake.png",
-        draw:true,
-        player1:
-            [
-                "Lyna Tanner",
-                "Roderic Flich",
-            ],
-        player2:
-            [
-                "Sledge Hammer",
-                "Willie Tanner",
-            ],
+        draw: true,
+        player1: "Lynn Tanner",
+        player2: "Roderick Flich",
+        player3: "Sledge Hammer",
+        player4: "Willie Tanner",
         time: "19"
     },
     {
         type: "./icons/TennisRackets.png",
         rating: "./icons/Handshake.png",
-        draw:true,
-        player1:
-            [
-                "Angus MacGyver",
-                "Hannibal Smith",
-            ],
-        player2:
-            [
-                "Michael Knight",
-                "Fred Decker",
-            ],
+        draw: true,
+        player1: "Angus MacGyver",
+        player2: "Hannibal Smith",
+        player3: "Michael Knight",
+        player4: "Fred Decker",
         time: "19"
     },
     {
         type: "./icons/TennisRackets.png",
         rating: "./icons/Handshake.png",
-        draw:true,
-        player1:
-            [
-                "Thomas Magnum",
-                "Roderick Decker",
-            ],
-        player2:
-            [
-                "Tony Danza",
-                "Batuhan Gören",
-            ],
+        draw: true,
+        player1: "Thomas Magnum",
+        player2: "Roderic Decker",
+        player3: "Tony Danza",
+        player4: "Batuhan Gören",
         time: "21"
     },
     {
         type: "./icons/TennisRacket.png",
         rating: "./icons/Handshake.png",
-        draw:true,
-        player1:
-            [
-                "Cristina Tanner",
-            ],
-        player2:
-            [
-                "Capt. Trunk"
-            ],
+        draw: true,
+        player1: "Cristian Tanner",
+        player2: "",
+        player3: "Capt. Trunk",
+        player4: "",
         time: "26"
     },
 ]
@@ -134,18 +88,18 @@ function updateSize() {
     if (window.innerWidth <= 550) {
         resizeList = waitingList.slice(0, 4)
 
-        if(executed){           
+        if (executed) {
             refresh()
         }
     }
     else {
         resizeList = waitingList
     }
-    if (resizeList.length >= 5) {        
+    if (resizeList.length >= 5) {
         document.getElementById('expandText').innerHTML = ""
         document.getElementById('expandArrow').innerHTML = ""
     }
-   
+
 }
 
 
@@ -159,32 +113,31 @@ buildMobileCard(waitingList)
 
 function buildTable(data) {
     var table = document.getElementById('waitingList')
-    var row =``
+    var row = ``
     for (var i = 0; i < data.length; i++) {
-
-        if(data[i].draw){       // Check if it is draw then put handshake image on the rating value
+        if (data[i].draw) {       // Check if it is draw then put handshake image on the rating value
             row = `
             <tr>
                 <td class="web"><img src="${data[i].type}"/></td>
                 <td class="web"><img src="${data[i].rating}"/></td>
-                <td >${data[i].player1.join('<br/><br/>')}</td>
-                <td >${data[i].player2.join('<br/><br/>')}</td>
-                <td> <span class="bolder">${data[i].time}<span/><span class="soft"> mins<span/><img class="trash" src="./icons/Trash.png"/></td>
-            </tr>    
-            `
-        }
-        else{                   // Otherwise put rating value
-            row = `
-            <tr>
-                <td class="web"><img src="${data[i].type}"/></td>
-                <td class="web"><span class="bolder">${data[i].rating}<span/></td>
-                <td >${data[i].player1.join('<br/><br/>')}</td>
-                <td >${data[i].player2.join('<br/><br/>')}</td>
+                <td >${data[i].player1}<br/><br/> ${data[i].player2}</td>
+                <td >${data[i].player3}<br/><br/><span class="selectedItem">${data[i].player4}</span></td>
                 <td><span class="bolder">${data[i].time}<span/><span class="soft"> mins<span/><img class="trash" src="./icons/Trash.png"/></td>
             </tr>    
             `
         }
-        
+        else {                   // Otherwise put rating value
+            row = `
+            <tr>
+                <td class="web"><img src="${data[i].type}"/></td>
+                <td class="web"><span class="bolder">${data[i].rating}<span/></td>
+                <td >${data[i].player1}<br/><br/> ${data[i].player2}</td>
+                <td >${data[i].player3}<br/><br/><span class="selectedItem">${data[i].player4}</span></td>
+                <td><span class="bolder">${data[i].time}<span/><span class="soft"> mins<span/><img class="trash" src="./icons/Trash.png"/></td>
+            </tr>    
+            `
+        }
+
         table.innerHTML += row
     }
 }
@@ -230,7 +183,7 @@ function buildCard(data) {
 
 function buildMobileCard(data) {
 
-    executed=true       // Which is activated mobile screen mode
+    executed = true       // Which is activated mobile screen mode
 
     var cards = document.getElementById('mobileCourtsGroup')
     var deactiveCards = 2
@@ -263,5 +216,14 @@ function buildMobileCard(data) {
         `
         cards.innerHTML += card
     }
-    
+
 }
+
+//SelectedItem Color Process
+document.querySelectorAll('.selectedItem').forEach(selectedItem => {
+
+    let item = selectedItem.textContent.indexOf("Batuhan Gören")
+    if (item !== -1) {
+        selectedItem.classList.add('red')
+    }
+})
